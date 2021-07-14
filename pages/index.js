7ca38f1580f7b10cd7acc848cb11ba62716f83c6
Luis Gustavo Monteiro <86658985+gustavostudent@users.noreply.github.com>
2021-07-14 07:15:18 -0300

    :tada: initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,63 @@
 import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu } from '../src/lib/AluraCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+
+function ProfileSideBar(props) {
+  return (
+    <Box >
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: "8px" }} />
+    </Box>
+  )
+}
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'GustavoMont'
+  const misAmi = [
+    'omariosouto',
+    'filipedeschamps',
+    'juunegreiros',
+    'peas',
+    'felipefialho',
+    'marcobrunodev'
+  ]
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>Bem Vindo</Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'relationsArea' }}>
+
+          <ProfileRelationsBoxWrapper>
+
+
+            <h2 className="smallTitle">Pessoas da Comunidade({misAmi.length})</h2>
+            <ul>
+              {misAmi.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+
+          </ProfileRelationsBoxWrapper>
+        </div>
+
+      </MainGrid>
+    </>
+  )
 }
