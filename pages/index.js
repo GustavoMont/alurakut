8b9cef3f7f7de1c1fe63ea656d524d2828d6e218
GustavoMont <lgmluisgm@gmail.com>
2021-07-14 09:30:57 -0300

    :tada: change OrkutNostalgic

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu } from '../src/lib/AluraCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AluraCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 
 
 function ProfileSideBar(props) {
   return (
-    <Box >
+    <Box>
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: "8px" }} />
+      
+      <hr/>
+
+      <a className="" href={`https://github.com/${props.githubUser}`}>LINK PIKA</a>
+
     </Box>
   )
 }
@@ -33,7 +38,10 @@ export default function Home() {
           <ProfileSideBar githubUser={githubUser} />
         </div>
         <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
-          <Box>Bem Vindo</Box>
+          <Box>
+            <h1 className="title">Bem Vindo</h1>
+            <OrkutNostalgicIconSet></OrkutNostalgicIconSet> 
+          </Box>
         </div>
         <div className="profileRelationsArea"  style={{ gridArea: 'relationsArea' }}>
 
